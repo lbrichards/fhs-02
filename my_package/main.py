24dfd51601a74736e9711d0cfd41c47a4929f932
Larry Richards <larry@source1.jp>
2018-09-25 14:30:44 +0900

    initial commit

---
+++ my_package/main.py
@@ -0,0 +1,8 @@
+from my_package.module1 import func1
+
+def main():
+    print(func1('hello'))    
+
+
+if __name__ == '__main__':
+    main()
---
+++ my_package/module1.py
@@ -0,0 +1,11 @@
+
+"""
+双方向インポートするとエラーが発生。
+
+from my_package.main import main
+"""
+
+
+
+def func1(arg1):
+    return arg1
